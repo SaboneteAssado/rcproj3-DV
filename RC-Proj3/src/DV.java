7bc72c12bff832d90c947760a158e4648e3ae89c
Miguel Araujo <miguel.santos.araujo@gmail.com>
2019-11-27 19:11:28 +0000

    Start v2.0

--- RC-Proj3/src/DV.java
+++ RC-Proj3/src/DV.java
@@ -10,7 +10,9 @@ public class DV implements RoutingAlgorithm
 	static final int INFINITY = 60;		// "infinity" metric
 	
 	Router thisRouter = null;  // the router were this algorithm is running
-
+	int interval = 0;
+	boolean pReverse = false, expire = false;
+	
 	// declare your routing table here using DVRoutingTableEntry (see end of this file)
 
 	public DV()
@@ -20,19 +22,21 @@ public class DV implements RoutingAlgorithm
 	public void setRouterObject(Router obj)
 	{
 		thisRouter = obj;
-
 	}
 
 	public void setUpdateInterval(int u)
 	{
+		interval = u;
 	}
 
 	public void setAllowPReverse(boolean flag)
 	{
+		pReverse = flag;
 	}
 
 	public void setAllowExpire(boolean flag)
 	{
+		expire = flag;
 	}
 
 	public void initalise()
@@ -72,29 +76,35 @@ public class DV implements RoutingAlgorithm
 class DVRoutingTableEntry implements RoutingTableEntry
 {
 
+	int d, i, m, t;
+	
 	public DVRoutingTableEntry(int d, int i, int m, int t)
 	{
+		this.d = d;
+		this.i = i;
+		this.m = m;
+		this.t = t;
 	}
 
-	public int getDestination() { return 0; }
+	public int getDestination() { return d; }
 
-	public void setDestination(int d) {  }
+	public void setDestination(int d) { this.d = d; }
 
-	public int getInterface() { return 0; }
+	public int getInterface() { return i; }
 
-	public void setInterface(int i) {  }
+	public void setInterface(int i) { this.i = i; }
 
-	public int getMetric() { return 0; }
+	public int getMetric() { return m; }
 
-	public void setMetric(int m) {  } 
+	public void setMetric(int m) { this.m = m; } 
 
-	public int getTime() { return 0; }
+	public int getTime() { return t; }
 
-	public void setTime(int t) {  }
+	public void setTime(int t) { this.t = t; }
 
 	public String toString() 
 	{
-		return "";
+		return "" + d + " " + i + " " + m + " " + t;
 	}
 }
 
